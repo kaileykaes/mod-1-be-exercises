7e52d12d79c409a0b86cac63a9cbb29fcaa2b17d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-05 18:27:13 -0700

    2 hours.

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -6,4 +6,28 @@ class Potluck
     @dishes = []
   end
 
+  def add_dish(dish)
+    @dishes << dish
+  end
+
+  def get_all_from_category(category)
+    category_dishes = []
+    @dishes.each do |dish|
+      category_dishes << dish if dish.category == category 
+    end
+  end
+
+  def menu 
+    potluck_menu = {}
+    app = []
+    ent = []
+    des = []
+    @dishes.each{|dish| app << dish.name if dish.category == :appetizer}
+    @dishes.each{|dish|ent << dish.name if dish.category == :entre}
+    @dishes.each{|dish|des << dish.name if dish.category == :dessert}
+    potluck_menu[:appetizers] = app.sort
+    potluck_menu[:entres] = ent.sort
+    potluck_menu[:desserts] = des.sort
+    potluck_menu 
+  end
 end
--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -3,8 +3,12 @@ require 'spec_helper'
 RSpec.describe Potluck do
   before(:each) do
     @potluck = Potluck.new("7-13-18")
+    @bean_dip = Dish.new("Bean Dip", :appetizer)
     @couscous_salad = Dish.new("Couscous Salad", :appetizer)
     @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    @summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    @roast_pork = Dish.new("Roast Pork", :entre)
+    @candy_salad = Dish.new("Candy Salad", :dessert)
   end
 
   it 'exists and has attributes' do
@@ -13,5 +17,35 @@ RSpec.describe Potluck do
     expect(@potluck.dishes).to eq([])
   end
 
+  it 'adds dishes' do 
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@cocktail_meatballs)
+    expect(@potluck.dishes.length).to eq(2)
+    expect(@potluck.dishes).to be_a(Array)
+  end
 
+  it 'gets all from a category' do
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
+    expect(@potluck.get_all_from_category(:appetizer)).to be_a(Array)
+    expect(@potluck.get_all_from_category(:appetizer).first).to eq(@couscous_salad)
+    expect(@potluck.get_all_from_category(:appetizer).first.name).to eq("Couscous Salad")
+  end
+
+  it 'stores menu by category' do 
+    @potluck.add_dish(@bean_dip)
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
+    expect(@potluck.menu).to be_a(Hash)
+    expect(@potluck.menu).to eq({:appetizers=>["Bean Dip", "Couscous Salad", "Summer Pizza"],
+      :entres=>["Cocktail Meatballs", "Roast Pork"],
+      :desserts=>["Candy Salad"]
+      })
+  end
 end
