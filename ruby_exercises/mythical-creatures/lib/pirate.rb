f1295e25d03f039340731e3c0e73ff4f6e12df46
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-09 16:12:46 -0700

    Feat: #booty

--- ruby_exercises/mythical-creatures/lib/pirate.rb
+++ ruby_exercises/mythical-creatures/lib/pirate.rb
@@ -1,12 +1,14 @@
 class Pirate
   attr_reader :name, 
               :job, 
-              :heinous_act
+              :heinous_act, 
+              :booty
   
   def initialize(name, job = 'Scallywag')
     @name = name
     @job = job
     @heinous_act = 0
+    @booty = 0
   end
 
   def commit_heinous_act
