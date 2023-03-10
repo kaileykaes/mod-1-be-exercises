d6c99791ccdb23465b02fe00576b57e3f6db8df8
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-09 19:12:18 -0700

    create files

--- ruby_exercises/mythical-creatures/lib/medusa.rb
+++ ruby_exercises/mythical-creatures/lib/medusa.rb
@@ -1,7 +1,17 @@
 class Medusa
+  attr_reader :name, 
+              :statues
+  
+  def initialize(name)
+    @name = name
+    @statues = []
+  end  
+end
+
+class Person
   attr_reader :name
   
   def initialize(name)
     @name = name
-  end  
+  end
 end
