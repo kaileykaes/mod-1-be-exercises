290c80713ed78450bcb8d2c17f11d8a11b1a085e
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-14 14:34:26 -0600

    Feat: #initialize & #sound

---
+++ ruby_exercises/initialize/lib/parrot.rb
@@ -0,0 +1,13 @@
+class Parrot
+  attr_reader :name, 
+              :known_words
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @known_words = attributes[:known_words]
+  end  
+
+  def sound
+    "Squawk!"
+  end
+end
