03dde1b8893c5e04c0646c45c878c17c8e3fc560
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-28 16:31:50 -0600

    Add #adopt feature and test

--- practice_independent_challenges/week3/pets_and_customers/lib/customer.rb
+++ practice_independent_challenges/week3/pets_and_customers/lib/customer.rb
@@ -8,4 +8,8 @@ class Customer
     @id = id
     @pets = []
   end  
+
+  def adopt(pet)
+    @pets << pet
+  end
 end
--- practice_independent_challenges/week3/pets_and_customers/spec/customer_spec.rb
+++ practice_independent_challenges/week3/pets_and_customers/spec/customer_spec.rb
@@ -1,42 +1,22 @@
-require './lib/customer'
-require './lib/pet'
+require 'spec_helper'
 
 RSpec.describe Customer do
   before(:each) do
-    @joel = Customer.new("Joel", 2)
+    @joel = Customer.new("Joel", 2) 
+    @samson = Pet.new({name: "Samson", type: :dog, age: 3})    
+    @lucy = Pet.new({name: "Lucy", type: :cat, age: 12})
   end
 
-  it "exists" do
-    expect(@joel).to be_instance_of(Customer)
-  end
-
-  it "has attributes" do
-    expect(@joel.name).to eq("Joel")
+  it 'exists and has attr' do
+    expect(@joel).to be_a Customer
+    expect(@joel.name).to eq('Joel')
     expect(@joel.id).to eq(2)
-  end
-
-  it "starts with no pets" do
     expect(@joel.pets).to eq([])
   end
 
-  it "can adopt new pets" do
-    samson = Pet.new({name: "Samson", type: :dog})
-    lucy = Pet.new({name: "Lucy", type: :cat})
-
-    @joel.adopt(samson)
-    @joel.adopt(lucy)
-    expect(@joel.pets).to eq([samson, lucy])
+  it '#adopt' do 
+    @joel.adopt(@samson)
+    @joel.adopt(@lucy) 
+    expect(@joel.pets).to eq([@samson, @lucy])
   end
-
-  it "has starting outstanding balance at 0" do
-    expect(@joel.outstanding_balance).to eq(0)
-  end
-
-  it "can charge to increase oustanding balance" do
-    @joel.charge(15)
-    @joel.charge(7)
-
-    expect(@joel.outstanding_balance).to eq(22)
-  end
-
 end
