dc06b30dbfe05e5a442bc91b56a4f9a1f175a9fa
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-28 16:24:46 -0600

    Test: #fed? & #feed

--- practice_independent_challenges/week3/pets_and_customers/spec/pet_spec.rb
+++ practice_independent_challenges/week3/pets_and_customers/spec/pet_spec.rb
@@ -1,26 +1,31 @@
-require './lib/pet'
+require 'spec_helper'
 
 RSpec.describe Pet do
   before(:each) do
-    @samson = Pet.new({name: "Samson", type: :dog})
+    @samson = Pet.new({name: "Samson", type: :dog, age: 3})    
   end
 
-  it "exists" do
-    expect(@samson).to be_instance_of(Pet)
+  describe '#initialize' do 
+    it 'exists' do
+      expect(@samson).to be_a(Pet)
+    end
+
+    it '#has attr' do 
+      expect(@samson.name).to eq('Samson')
+      expect(@samson.type).to eq(:dog)
+      expect(@samson.age).to eq(3)
+    end
   end
 
-  it "has attributes" do
-    expect(@samson.name).to eq("Samson")
-    expect(@samson.type).to eq(:dog)
+  describe 'feeding' do 
+    it '#fed?' do 
+      expect(@samson.fed?).to be false
+    end
+    
+    it '#feed' do 
+      @samson.feed
+      expect(@samson.fed?).to be true
+  
+    end
   end
-
-  it "is not fed by default" do
-    expect(@samson.fed?).to be(false)
-  end
-
-  it "can be fed" do
-    @samson.feed
-    expect(@samson.fed?).to be(true)
-  end
-
 end
