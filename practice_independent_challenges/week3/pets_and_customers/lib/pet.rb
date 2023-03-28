f08055430449b9f26b5a97cecce701da409ecd7b
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-28 16:26:34 -0600

    Feat: #fed? & #feed

--- practice_independent_challenges/week3/pets_and_customers/lib/pet.rb
+++ practice_independent_challenges/week3/pets_and_customers/lib/pet.rb
@@ -1,11 +1,21 @@
 class Pet
   attr_reader :name, 
               :type, 
-              :age
+              :age, 
+              :eaten
   
   def initialize(attributes)
     @name = attributes[:name]
     @type = attributes[:type]
     @age = attributes[:age]
+    @eaten = false
   end  
+
+  def fed? 
+    @eaten
+  end
+
+  def feed
+    @eaten = true
+  end
 end
