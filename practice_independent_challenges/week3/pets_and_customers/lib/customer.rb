7d9129330fd6dbf1b2bcbcaf06867e4205854cf7
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-28 16:29:21 -0600

    Feat: #initialize

--- practice_independent_challenges/week3/pets_and_customers/lib/customer.rb
+++ practice_independent_challenges/week3/pets_and_customers/lib/customer.rb
@@ -1,21 +1,11 @@
 class Customer
   attr_reader :name,
-              :id,
-              :pets,
-              :outstanding_balance
-
+              :id, 
+              :pets
+  
   def initialize(name, id)
     @name = name
     @id = id
     @pets = []
-    @outstanding_balance = 0
-  end
-
-  def adopt(pet)
-    @pets << pet
-  end
-
-  def charge(amount)
-    @outstanding_balance += amount
-  end
+  end  
 end
