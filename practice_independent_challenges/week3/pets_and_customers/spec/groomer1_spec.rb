5034e17c1149a3bf735a051ac5f8333b35328825
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-27 20:06:11 -0600

    TesT: has attributes

--- practice_independent_challenges/week3/pets_and_customers/spec/groomer1_spec.rb
+++ practice_independent_challenges/week3/pets_and_customers/spec/groomer1_spec.rb
@@ -2,12 +2,16 @@ require 'spec_helper'
 
 RSpec.describe Groomer do
   before(:each) do
-    @groomer = Groomer.new(name)
+    @groomer = Groomer.new('Judy')
   end
 
   describe '#initialize' do 
     it 'exists' do
       expect(@groomer).to be_a Groomer
+    end
+
+    it 'has attribute' do 
+      expect(@groomer.name).to eq('Judy')
     end
   end
 
