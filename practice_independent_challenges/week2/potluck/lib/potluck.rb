e8ed7a4cdd6b3ab143baa68959abe80a4d2e9199
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-28 15:59:50 -0600

    Feat: #get_all_from_category

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -6,4 +6,15 @@ class Potluck
     @date = date
     @dishes = []
   end  
+
+  def add_dish(dish)
+    @dishes << dish
+  end
+
+  def get_all_from_category(category)
+    category_dishes = @dishes.find_all do |dish|
+      dish.category == category
+    end
+    category_dishes
+  end
 end
