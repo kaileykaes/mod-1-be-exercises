bfa9d8fbd7750eb5e19df56634934d87264730d6
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-28 15:59:24 -0600

    Test: #get_all_from_category

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Potluck do
     @potluck = Potluck.new("7-13-18")
     @couscous_salad = Dish.new("Couscous Salad", :appetizer)
     @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    @summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    @roast_pork = Dish.new("Roast Pork", :entre)
+    @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    @candy_salad = Dish.new("Candy Salad", :dessert)
   end
 
   it 'exists & has attributes' do
@@ -18,5 +22,18 @@ RSpec.describe Potluck do
     @potluck.add_dish(@cocktail_meatballs)
     expect(@potluck.dishes).to eq([@couscous_salad, @cocktail_meatballs])
     expect(@potluck.dishes.length).to eq(2)
+  end
+
+  it '#get_all_from_category' do 
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
+    expect(@potluck.get_all_from_category(:appetizer)).to eq([@couscous_salad, @summer_pizza])
+    expect(@potluck.get_all_from_category(:appetizer).first).to eq(@couscous_salad)
+    expect(@potluck.get_all_from_category(:appetizer).first.name).to eq('Couscous Salad')
+
+
   end
 end
