9f816cc442ae0eafc2a01d707d019da596299c86
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-29 12:00:35 -0600

    oustanding_balances and pet count by type

--- practice_independent_challenges/week3/pets_and_customers/lib/groomer1.rb
+++ practice_independent_challenges/week3/pets_and_customers/lib/groomer1.rb
@@ -10,4 +10,22 @@ class Groomer
   def add_customer(customer)
     customers << customer
   end
+
+  def customers_with_oustanding_balances
+    indebted_customers = customers.find_all do |customer|
+      customer.outstanding_balance > 0
+    end
+    indebted_customers
+  end
+
+  def count_pets_by_type(type)
+    all_pets = []
+    customers.each do |customer|
+      all_pets << customer.pets
+    end
+    type_pets = all_pets.flatten.find_all do |pet|
+      pet.type == type
+    end
+    type_pets.length
+  end
 end
--- practice_independent_challenges/week3/pets_and_customers/spec/groomer1_spec.rb
+++ practice_independent_challenges/week3/pets_and_customers/spec/groomer1_spec.rb
@@ -3,9 +3,30 @@ require 'spec_helper'
 RSpec.describe Groomer do
   before(:each) do
     @groomer = Groomer.new('Judy')
+
     @joel = Customer.new("Joel", 2) 
+    @mary = Customer.new('Mary', 5)
+    @camry = Customer.new('Camry', 1)
+    @jolene = Customer.new('Jolene', 3)
+    @jr = Customer.new('J.R', 4)
+
     @samson = Pet.new({name: "Samson", type: :dog, age: 3})    
     @lucy = Pet.new({name: "Lucy", type: :cat, age: 12})
+    @kevin = Pet.new({name: "Kevin", type: :dog, age: 4})    
+    @paddington = Pet.new({name: "Paddington", type: :bear, age: 5})
+    @nebraska = Pet.new({name: "Nebraska", type: :cat, age: 7})    
+    @falafel = Pet.new({name: "Falafel", type: :dog, age: 10})
+    @johnson = Pet.new({name: "Johnson", type: :ferret, age: 3})    
+    @hoosier = Pet.new({name: "Hoosier", type: :cat, age: 17})
+   
+    @joel.adopt(@samson)
+    @joel.adopt(@lucy) 
+    @mary.adopt(@paddington)
+    @camry.adopt(@kevin)  
+    @camry.adopt(@hoosier)
+    @jolene.adopt(@nebraska) 
+    @jr.adopt(@johnson)
+    @jr.adopt(@falafel)
   end
 
   it 'exists' do
@@ -22,6 +43,34 @@ RSpec.describe Groomer do
 
   it 'can add customers' do 
     @groomer.add_customer(@joel)
-    expect(@groomer.customers).to eq([@joel])
+    @groomer.add_customer(@camry)
+    @groomer.add_customer(@jolene)
+    @groomer.add_customer(@mary)
+    @groomer.add_customer(@jr)
+    expect(@groomer.customers).to eq([@joel, @camry, @jolene, @mary, @jr])
+  end
+
+  it 'can find customers with outstanding balances' do 
+    @groomer.add_customer(@joel)
+    @groomer.add_customer(@camry)
+    @groomer.add_customer(@jolene)
+    @groomer.add_customer(@mary)
+    @groomer.add_customer(@jr)
+    @joel.charge(13)
+    @camry.charge(30)
+    @mary.charge(500)
+    expect(@groomer.customers_with_oustanding_balances).to eq([@joel, @camry, @mary])
+  end
+
+  it 'can count pets by type' do 
+    @groomer.add_customer(@joel)
+    @groomer.add_customer(@camry)
+    @groomer.add_customer(@jolene)
+    @groomer.add_customer(@mary)
+    @groomer.add_customer(@jr)
+    expect(@groomer.count_pets_by_type(:dog)).to eq(3)
+    expect(@groomer.count_pets_by_type(:ferret)).to eq(1)
+    expect(@groomer.count_pets_by_type(:bear)).to eq(1)
+    expect(@groomer.count_pets_by_type(:cat)).to eq(3)
   end
 end
