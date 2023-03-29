aa1c2252543cb9fd29f3e064c93ec4690aea436d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-29 11:27:04 -0600

    initialize

--- practice_independent_challenges/week3/pets_and_customers/lib/groomer1.rb
+++ practice_independent_challenges/week3/pets_and_customers/lib/groomer1.rb
@@ -1,17 +1,7 @@
 class Groomer
-  attr_reader :name, 
-              :customers
+  attr_reader :name
   
   def initialize(name)
     @name = name
-    @customers = Hash.new {|hash_obj, key| hash_obj[key] = []}
-  end
-
-  def add_customer(customer)
-    @customers['Alice'] = 0 
-  end
-
-  def charge_customer(customer, cost)
-    @customers[customer] << cost.delete('$').to_f.round(2)
   end
 end
--- practice_independent_challenges/week3/pets_and_customers/spec/groomer1_spec.rb
+++ practice_independent_challenges/week3/pets_and_customers/spec/groomer1_spec.rb
@@ -5,29 +5,11 @@ RSpec.describe Groomer do
     @groomer = Groomer.new('Judy')
   end
 
-  describe '#initialize' do 
-    it 'exists' do
-      expect(@groomer).to be_a Groomer
-    end
-
-    it 'has attributes' do 
-      expect(@groomer.name).to eq('Judy')
-      expect(@groomer.customers).to be_a Hash
-    end
-  end
-  
-  describe '#add_customer' do 
-    it 'can add a customer' do 
-      @groomer.add_customer('Alice')
-      expect(@groomer.customers).to eq({'Alice' => 0})
-    end
+  it 'exists' do
+    expect(@groomer).to be_a Groomer
   end
 
-  describe '#charge_customer' do 
-    it 'can charge a customer' do 
-      @groomer.charge_customer('Alice', '$32.50')
-      expect(@groomer.charge_customer('Alice', '$60.30'))
-      expect(@groomer.customers).to eq({'Alice' => [32.50, 60.30]})
-    end
+  it 'has a name' do 
+    expect(@groomer.name).to eq('Judy')
   end
 end
