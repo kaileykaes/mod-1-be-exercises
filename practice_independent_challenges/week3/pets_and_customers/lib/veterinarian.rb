b6f3bdcbc214f3607c74c57f0e6f83dac10f332e
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-29 14:52:04 -0600

    veterinarian

--- practice_independent_challenges/week3/pets_and_customers/lib/veterinarian.rb
+++ practice_independent_challenges/week3/pets_and_customers/lib/veterinarian.rb
@@ -1,7 +1,27 @@
 class Veterinarian
-  attr_reader :name
+  attr_reader :name, 
+              :customers
   
   def initialize(name)
     @name = name
+    @customers = []
+  end
+
+  def add_customer(customer)
+    @customers << customer
+  end
+
+  def sort_pets
+    pets = @customers.map do |customer|
+      customer.pets
+    end
+    sorted_pets = pets.flatten.sort_by do |pet|
+      pet.age
+    end
+    sorted_pets.reverse
+  end
+
+  def count_pets(customer)
+    customer.pets.length
   end
 end
--- practice_independent_challenges/week3/pets_and_customers/spec/veterinarian_spec.rb
+++ practice_independent_challenges/week3/pets_and_customers/spec/veterinarian_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Veterinarian do
     @jolene = Customer.new('Jolene', 3)
     @jr = Customer.new('J.R', 4)
 
-    @samson = Pet.new({name: "Samson", type: :dog, age: 3})    
+    @samson = Pet.new({name: "Samson", type: :dog, age: 2})    
     @lucy = Pet.new({name: "Lucy", type: :cat, age: 12})
     @kevin = Pet.new({name: "Kevin", type: :dog, age: 4})    
     @paddington = Pet.new({name: "Paddington", type: :bear, age: 5})
@@ -18,6 +18,15 @@ RSpec.describe Veterinarian do
     @falafel = Pet.new({name: "Falafel", type: :dog, age: 10})
     @johnson = Pet.new({name: "Johnson", type: :ferret, age: 3})    
     @hoosier = Pet.new({name: "Hoosier", type: :cat, age: 17})
+
+    @joel.adopt(@samson)
+    @joel.adopt(@lucy) 
+    @mary.adopt(@paddington)
+    @camry.adopt(@kevin)  
+    @camry.adopt(@hoosier)
+    @jolene.adopt(@nebraska) 
+    @jr.adopt(@johnson)
+    @jr.adopt(@falafel)
   end
 
   it 'exists' do
@@ -26,5 +35,54 @@ RSpec.describe Veterinarian do
 
   it 'has a name' do 
     expect(@veterinarian.name).to eq('Dr. Dodson')
+  end
+
+  it 'starts without clientele' do 
+    expect(@veterinarian.customers).to eq([])
+  end
+
+  it 'can add customers' do 
+    @veterinarian.add_customer(@joel)
+    @veterinarian.add_customer(@mary)
+    @veterinarian.add_customer(@camry)
+    @veterinarian.add_customer(@jolene)
+    @veterinarian.add_customer(@jr)
+    expect(@veterinarian.customers).to eq([@joel, @mary, @camry, @jolene, @jr])
+  end
+
+  it 'customers can have pets' do 
+    @veterinarian.add_customer(@joel)
+    @veterinarian.add_customer(@mary)
+    @veterinarian.add_customer(@camry)
+    @veterinarian.add_customer(@jolene)
+    @veterinarian.add_customer(@jr)
+    expect(@veterinarian.customers[0].pets).to eq([@samson, @lucy])
+    expect(@veterinarian.customers[1].pets).to eq([@paddington])
+    expect(@veterinarian.customers[2].pets).to eq([@kevin, @hoosier])
+    expect(@veterinarian.customers[3].pets).to eq([@nebraska])
+    expect(@veterinarian.customers[4].pets).to eq([@johnson, @falafel])
+  end
+
+  it 'can find all pets sorted by age' do
+    @veterinarian.add_customer(@joel)
+    @veterinarian.add_customer(@mary)
+    @veterinarian.add_customer(@camry)
+    @veterinarian.add_customer(@jolene)
+    @veterinarian.add_customer(@jr)
+    expect(@veterinarian.sort_pets).to eq([@hoosier, @lucy, @falafel, @nebraska, @paddington, @kevin, @johnson, @samson])
+  end
+
+  it 'can count pets of customers' do 
+    @veterinarian.add_customer(@joel)
+    @veterinarian.add_customer(@mary)
+    @veterinarian.add_customer(@camry)
+    @veterinarian.add_customer(@jolene)
+    @veterinarian.add_customer(@jr)
+    expect(@veterinarian.count_pets(@joel)).to eq(2)
+    expect(@veterinarian.count_pets(@mary)).to eq(1)
+    expect(@veterinarian.count_pets(@camry)).to eq(2)
+    expect(@veterinarian.count_pets(@jolene)).to eq(1)
+    expect(@veterinarian.count_pets(@jr)).to eq(2)
+
   end
 end
