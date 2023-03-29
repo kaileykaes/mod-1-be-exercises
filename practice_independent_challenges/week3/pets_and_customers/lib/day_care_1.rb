160df3b3814bcd53a21fbdc435e6e57439c4e207
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-29 13:39:40 -0600

    create daycare class and associated methods

--- practice_independent_challenges/week3/pets_and_customers/lib/day_care_1.rb
+++ practice_independent_challenges/week3/pets_and_customers/lib/day_care_1.rb
@@ -1,7 +1,30 @@
 class DayCare
-  attr_reader :name
+  attr_reader :name, 
+              :customers
   
   def initialize(name)
     @name = name
+    @customers = []
+  end
+
+  def add_customer(customer)
+    @customers << customer
+  end
+
+  def find_by_id(id)
+    got_it = customers.find do |customer|
+      customer.id == id
+    end
+    got_it
+  end
+
+  def unfed_pets
+    pets = customers.map do |customer|
+      customer.pets
+    end
+    unfed = pets.flatten.find_all do |pet|
+      pet.fed? != true
+    end
+    unfed
   end
 end
--- practice_independent_challenges/week3/pets_and_customers/spec/day_care_1_spec.rb
+++ practice_independent_challenges/week3/pets_and_customers/spec/day_care_1_spec.rb
@@ -32,6 +32,61 @@ RSpec.describe DayCare do
 
   it 'exists' do
     expect(@daycare).to be_a DayCare
-    expect(@daycare_2).to be_a DayCare
+  end
+
+  it 'has a name' do 
+    expect(@daycare.name).to eq('Bark Bliss')
+  end
+
+  it 'starts without customers' do 
+    expect(@daycare.customers).to eq([])
+  end
+
+  it 'can add customers' do 
+    @daycare.add_customer(@joel)
+    @daycare.add_customer(@mary)
+    @daycare.add_customer(@camry)
+    @daycare.add_customer(@jolene)
+    @daycare.add_customer(@jr)
+    expect(@daycare.customers).to eq([@joel, @mary, @camry, @jolene, @jr])
+  end
+
+  it 'customers can have pets' do 
+    @daycare.add_customer(@joel)
+    @daycare.add_customer(@mary)
+    @daycare.add_customer(@camry)
+    @daycare.add_customer(@jolene)
+    @daycare.add_customer(@jr)
+    expect(@daycare.customers[0].pets).to eq([@samson, @lucy])
+    expect(@daycare.customers[1].pets).to eq([@paddington])
+    expect(@daycare.customers[2].pets).to eq([@kevin, @hoosier])
+    expect(@daycare.customers[3].pets).to eq([@nebraska])
+    expect(@daycare.customers[4].pets).to eq([@johnson, @falafel])
+  end
+
+  it 'finds customers by id' do 
+    @daycare.add_customer(@joel)
+    @daycare.add_customer(@mary)
+    @daycare.add_customer(@camry)
+    @daycare.add_customer(@jolene)
+    @daycare.add_customer(@jr)
+    expect(@daycare.find_by_id(1)).to eq(@camry)
+    expect(@daycare.find_by_id(2)).to eq(@joel)
+    expect(@daycare.find_by_id(3)).to eq(@jolene)
+    expect(@daycare.find_by_id(4)).to eq(@jr)
+    expect(@daycare.find_by_id(5)).to eq(@mary)
+  end
+
+  it 'finds unfed pets' do 
+    @daycare.add_customer(@joel)
+    @daycare.add_customer(@mary)
+    @daycare.add_customer(@camry)
+    @daycare.add_customer(@jolene)
+    @daycare.add_customer(@jr)
+    @samson.feed
+    @lucy.feed
+    @paddington.feed
+    @nebraska.feed
+    expect(@daycare.unfed_pets).to eq([@kevin, @hoosier, @johnson, @falafel])
   end
 end
