0ef1772078ef6adb7a57adb80906779e03e7dc60
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-30 13:13:41 -0600

    Test: #eliminated, #eliminated?, #add_players

--- practice_independent_challenges/week3/world_cup/spec/team_spec.rb
+++ practice_independent_challenges/week3/world_cup/spec/team_spec.rb
@@ -2,7 +2,15 @@ require 'spec_helper'
 
 RSpec.describe Team do
   before(:each) do
-    @team = Team.new("France")    
+    @team = Team.new("France")  
+    @mbappe = Player.new({
+      name: "Kylian Mbappe", 
+      position: "forward"
+    })
+    @pogba = Player.new({
+      name: "Paul Pogba", 
+      position: "midfielder"
+    })    
   end
 
   it 'exists' do
@@ -12,4 +20,21 @@ RSpec.describe Team do
   it 'has attributes' do 
     expect(@team.country).to eq('France')
   end
+
+  it 'can be eliminated' do 
+    expect(@team.eliminated?).to be false
+    @team.eliminated = true
+    expect(@team.eliminated?).to be true
+  end
+
+  it 'starts with no players' do 
+    expect(@team.players).to eq([])
+  end
+
+  it 'can add players' do 
+    @team.add_player(@mbappe)
+    @team.add_player(pogba)
+    expect(@team.players).to eq([@mbappe, @pogba])
+  end
+
 end
