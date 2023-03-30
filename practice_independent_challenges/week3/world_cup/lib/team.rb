38d5a4859953de4c936c50b3f5fa52b6193283e3
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-30 13:14:00 -0600

    Feat: #eliminated, #eliminated?

--- practice_independent_challenges/week3/world_cup/lib/team.rb
+++ practice_independent_challenges/week3/world_cup/lib/team.rb
@@ -1,7 +1,13 @@
 class Team
-  attr_reader :country
+  attr_reader :country 
+  attr_accessor  :eliminated
   
   def initialize(country)
     @country = country
+    @eliminated = false
+  end
+
+  def eliminated?
+    @eliminated
   end
 end
