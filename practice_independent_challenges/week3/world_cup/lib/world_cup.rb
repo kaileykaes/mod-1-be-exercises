078b86ca5ec5078749ce65270ae413ae41d165af
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-30 13:56:02 -0600

    #active_players_by_position

--- practice_independent_challenges/week3/world_cup/lib/world_cup.rb
+++ practice_independent_challenges/week3/world_cup/lib/world_cup.rb
@@ -6,4 +6,14 @@ class WorldCup
     @year = year
     @teams = teams
   end
+
+  def active_players_by_position(position)
+    players = []
+    @teams.each do |team|
+      players << team.players unless team.eliminated?
+    end
+    position_players = players.flatten.find_all do |player|
+      player.position == position
+    end
+  end
 end
