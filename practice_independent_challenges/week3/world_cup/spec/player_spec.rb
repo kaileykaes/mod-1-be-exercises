60488857e9cb444badd94ae51f26fa6400d83d3c
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-30 13:04:32 -0600

    Test: #initialize

---
+++ practice_independent_challenges/week3/world_cup/spec/player_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe Player do
+  before(:each) do
+    @player = Player.new({name: "Luka Modric", position: "midfielder"})  
+  end
+
+  it 'exists' do
+    expect(@player).to be_a Player
+  end
+
+  it 'has attributes' do 
+    expect(@player.name).to eq('Luka Modric')
+    expect(@player.position).to eq('midfielder')
+  end
+end
